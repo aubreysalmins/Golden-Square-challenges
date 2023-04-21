832c5c82575cdd37c414e78c910914e8c2ad16ed
Aubrey Salmins <aubreysalmins@gmail.com>
2023-04-21 09:07:44 +0100

    Commit todo list file

---
+++ 08_test_drive_a_class_system/lib/todo_list.rb
@@ -0,0 +1,35 @@
+require "todo"
+
+class TodoList
+  def initialize
+    @todos = []
+  end
+
+  def add(todo)
+    @todos << todo
+  end
+
+  def incomplete
+    incompleted = []
+    @todos.each { |todo|
+      if todo.done? == false
+        incompleted << todo
+      end
+    }
+    return incompleted
+  end
+
+  def complete
+    completed = []
+    @todos.each { |todo|
+      if todo.done? == true
+        completed << todo
+      end
+    }
+    return completed
+  end
+
+  def give_up!
+    # Marks all todos as complete
+  end
+end
