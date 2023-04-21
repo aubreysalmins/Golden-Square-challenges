aa3cff535781645c2ab6ea9ea30f2635741f27f6
Aubrey Salmins <aubreysalmins@gmail.com>
2023-04-21 09:16:02 +0100

    Add more integration tests

--- 08_test_drive_a_class_system/spec/todo_list_integration_spec.rb
+++ 08_test_drive_a_class_system/spec/todo_list_integration_spec.rb
@@ -13,11 +13,34 @@ RSpec.describe "integration" do
     end
   end
 
-  it "returns all complete todos" do
+  it "returns completed todos" do
     todo_list = TodoList.new
     todo = Todo.new("Walk dog")
     todo_list.add(todo)
     expect(todo_list.complete).to eq []
   end
+
+  it "returns an incomplete task" do
+    todo_list = TodoList.new
+    todo = Todo.new("Walk dog")
+    todo_list.add(todo)
+    todo1 = Todo.new("Wash car")
+    todo_list.add(todo1)
+    todo.mark_done!
+    expect(todo_list.incomplete).to eq [todo1]
+  end
+
+  it "returns multiple completed tasks" do
+    todo_list = TodoList.new
+    todo = Todo.new("Walk dog")
+    todo_list.add(todo)
+    todo1 = Todo.new("Wash car")
+    todo_list.add(todo1)
+    todo.mark_done!
+    todo1.mark_done!
+    expect(todo_list.complete).to eq [todo, todo1]
+  end
+
+  
 end
 
