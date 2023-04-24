be0e2010ba76e214c639548cd9d643a46cf4db6e
Aubrey Salmins <aubreysalmins@gmail.com>
2023-04-24 08:53:54 +0100

    Write first integration test

--- mocking_bites/01_unit_testing_parent_child_classes_bite/spec/music_library_integration_spec.rb
+++ mocking_bites/01_unit_testing_parent_child_classes_bite/spec/music_library_integration_spec.rb
@@ -1,7 +1,13 @@
 require "music_library"
 require "track"
 
-RSpec.describe do
-  
-  
+RSpec.describe "integration" do
+  xit "adds and lists tracks" do
+    music_library = MusicLibrary.new
+    track_1 = Track.new("title1", "artist1")
+    track_2 = Track.new("title2", "artist2")
+    music_library.add(track_1)
+    music_library.add(track_2)
+    expect(music_library.all).to eq [track_1, track_2]
+  end
 end
---
+++ mocking_bites/01_unit_testing_parent_child_classes_bite/spec/music_library_spec.rb
@@ -0,0 +1,7 @@
+require "music_library"
+
+RSpec.describe MusicLibrary do
+  it "adds and lists tracks" do
+    
+  end
+end
